507a3c62277b2ce2fd6b58aa1b7109970c1c75ae
tylergotz <tyler.gotz@cuw.edu>
2016-05-02 13:52:14 -0500

    Update chatServer.c

--- chatServer.c
+++ chatServer.c
@@ -5,70 +5,76 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "sem.h"
+#include <semaphore.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 void setupServerSocket();
-void broadcast(char* msg, int* clients, int numClients);
+void broadcast(char* msg);
 void sendMessage(char* msg, int client);
 void childStuff(int clientfd);
 void receiveMessage(int clientfd);
+void processChat();
 
 struct sockaddr_in* server;
-uint16_t port = 3000;
+uint16_t port = 4000;
 int sockfd;
 socklen_t serverSize;
+int clientsID;
+int numberOfConnectedClientsID;
 
 int main(int argc, char** argv)
 {
     server = malloc(sizeof(struct sockaddr_in));
     int clientfd;
+   
+    //create share mem
     int MAX_CLIENTS = 1000;
-    int* clients = malloc(MAX_CLIENTS * sizeof(int));
-    int numberOfConnectedClients = 0;
+    clientsID = shmget(IPC_PRIVATE, MAX_CLIENTS * sizeof(int), IPC_CREAT | 0666);
+    numberOfConnectedClientsID = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+    
+    //link to shared mem
+    int* clients = shmat(clientsID, NULL, 0);
+    int* numberOfConnectedClients = shmat(numberOfConnectedClientsID, NULL, 0);
     sem_t block;
     sem_init(&block, 0, 1);
     
-    char* message = "Hello";
+    char* message = "hello";
     int pid;
-    
+    //setup the socket server
     setupServerSocket();
     
-    while(1)
-    {
-        printf("Listening....\n");
-        clientfd = accept(sockfd, (struct sockaddr *)server, &serverSize);
-        printf("Client Connected..... %d\n", clientfd);
-        clients[numberOfConnectedClients++] = clientfd;
-        
-        sem_wait(&block);
-        int i;
-        for(i = 0; i < numberOfConnectedClients; i++)
-        {
-            if(clients[i] != NULL)
-            {
-                //send message to all clients
-                sendMessage((char*) receiveMessage(clientfd), clients[i]);
-            }
-        }
-        
-        sem_post(&block);
-        pid = fork();
-        if(pid == 0)
-        {
-            break;
-        }
-    }
-        
+   while(1)
+   {
+    printf("Listening....\n");
+    clientfd = accept(sockfd ,  (struct sockaddr *)server , &serverSize);
+    printf("Client Connected.... %d\n", clientfd);
+    
+    //add this client to our array of clients
+    sem_wait(&block);
+    clients[(*numberOfConnectedClients)++] = clientfd;
+    sem_post(&block);
+    
+    processChat();
+    pid = fork();
+    
     if(pid == 0)
     {
-        childStuff(clientfd);
+        break;
     }
+   }
+   
+   //only do this if we are the child and have broken out of the infinite loop
+   if(pid == 0)
+   {
+       childStuff(clientfd);
+   }
 }
+
 
 void childStuff(int clientfd)
 {
-    printf("Child Stuff\n");
-    sendMessage("Hello from the child. Do you have candy?", clientfd);
+    sendMessage("Hello from the child.  Do you have candy?", clientfd);
     receiveMessage(clientfd);
 }
 
@@ -77,7 +83,7 @@ void receiveMessage(int clientfd)
     int MAX_SIZE = 2000 * sizeof(char);
     char* server_reply = malloc(MAX_SIZE);
     int error = recv(clientfd, server_reply, MAX_SIZE, 0);
-    puts(server_reply);
+    broadcast(server_reply);
 }
 
 void setupServerSocket()
@@ -85,30 +91,28 @@ void setupServerSocket()
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd == -1)
     {
-        puts("Bad Socket!!!!!");
+        puts("Bad Socket!!!!");
     }
     
     server->sin_family = AF_INET;
     server->sin_addr.s_addr = inet_addr("127.0.0.1");
     server->sin_port = htons( port );
     memset(server->sin_zero, '\0', sizeof server->sin_zero);
-    
+
     int yes = 1;
-    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
-    {
-        perror("setsockopt");
-        exit(1);
-    }
-    
-    serverSize = sizeof(server);
-    int error = bind(sockfd, (struct sockaddr* )server, sizeof(struct sockaddr));
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+		perror("setsockopt");
+		exit(1);
+	}
+	serverSize = sizeof(server);
+	
+	int error = bind(sockfd, (struct sockaddr *)server, sizeof(struct sockaddr));
     if(error < 0)
     {
         puts("Problem with binding...\n");
     }
     
-    error - listen(sockfd, 100);
-
+    error = listen(sockfd, 100);
 }
 
 void sendMessage(char* msg, int client)
@@ -116,17 +120,43 @@ void sendMessage(char* msg, int client)
     puts("Sending a message");
     printf("Size of msg is: %d\n", (int)sizeof(*msg));
     printf("Size of msg is: %d\n", (int)strlen(msg));
-    send(client, msg, strlen(msg), 0);
-    putts("Message Sent");
+    send(client , msg , strlen(msg) , 0);
+    puts("Message Sent");
 }
 
-void broadcast(char* msg, int* clients, int numClients)
+void processChat()
 {
+    char* command;
+    char* who;
+    char* msg;
+    
+    scanf("%ms", &command);
+    if(strcmp("tell", command) == 0)
+    {
+        scanf("%ms %m[^\n]", &who, &msg);
+        printf("Sending private message (%s) to: %s\n", msg, who);
+    }
+    else
+    {
+        scanf(%m[^\n], &msg);
+        char* destination = malloc(sizeof(command) + sizeof(msg));
+        strcpy(destination, command);
+        strcat(destination, msg);
+        printf("Broadcating: %s\n", destination);
+    }
+}
+
+void broadcast(char* msg)
+{
+    int* clients = shmat(clientsID, NULL, 0);
+    int* numberOfConnectedClients = shmat(numberOfConnectedClientsID, NULL, 0);
+    
     int i;
-    for(i = 0; i < numClients; i++)
+    for(i = 0; i < *numberOfConnectedClients; i++)
     {
         printf("Trying to send to client with FD: %d\n", clients[i]);
-        send(clients[i], msg, strlen(msg), 0);
+        //printf("Trying to send to client with FD: %d\n", *(clients + (i * sizeof(int))));
+        send(clients[i] , msg , strlen(msg) , 0);
         puts("sent");
     }
 }
