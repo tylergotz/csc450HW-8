db715c82ef1ab2557580b2142882d27250a570bb
tylergotz <tyler.gotz@cuw.edu>
2016-05-02 13:53:01 -0500

    Update chatClient.c

--- chatClient.c
+++ chatClient.c
@@ -4,17 +4,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
 
 int main(int argc, char** argv)
 {
-    uint16_t port = 3000;
-    int error; 
+    uint16_t port = 4000;
+    int error;
     struct sockaddr_in* server = malloc(sizeof(struct sockaddr_in));
     server->sin_addr.s_addr = inet_addr("127.0.0.1");
     server->sin_family = AF_INET;
     server->sin_port = htons( port );
-    char buffer [1024];
-    char buff[1024];
+    //memset(server->sin_zero, '\0', sizeof server->sin_zero);
+    
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     error = connect(sockfd, (struct sockaddr *)server, sizeof(struct sockaddr));
     if(error < 0)
@@ -24,11 +26,12 @@ int main(int argc, char** argv)
     else
     {
         puts("We are Connected");
-        
-        char* server_reply = malloc(2000 * sizeof(char));
+        //Receive a reply from the server
+        int MAX_SIZE = 2000 * sizeof(char);
+        char* server_reply = malloc(MAX_SIZE);
         while(1)
         {
-            error = recv(sockfd, server_reply, sizeof(server_reply), 0);
+            error = recv(sockfd, server_reply, MAX_SIZE, 0);
             if(error < 0)
             {
                 puts("recv failed");
@@ -37,17 +40,12 @@ int main(int argc, char** argv)
             {
                 puts("Reply received\n");
                 puts(server_reply);
-                num = recv(sockfd, buffer, sizeof(buffer), 0);
-                if(num<=0)
-                {
-                    printf("Either connection closed or error\n");
-                    break;
-                }
                 
-                buff[num] = '\0';
-                printf("Client:Message Received From Server -  %s\n",buffer);
+                char* message = "yo\tyo";
+                send(sockfd, message , strlen(message) , 0);
             }
         }
+        
     }
     free(server);
     return 0;
